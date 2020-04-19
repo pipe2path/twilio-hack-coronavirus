4feb1851390cb68236ed4c8944febb92434b8602
kevinmenezes <kevinmenezes@yahoo.com>
2020-04-19 14:22:33 -0700

    search page features

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../services/search.service';
+import { HttpHeaders } from '@angular/common/http';
+import { Item } from '../models/itemModel';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+  providers: [ SearchService]
+})
+export class SearchComponent implements OnInit {
+
+  public items;
+  public itemsDisplayed;
+  public itemsNeeded: Item[];
+  public name;
+  public phone;
+  public newName;
+  public newBrand;
+
+  constructor(private searchService: SearchService) { }
+
+  ngOnInit() {
+    this.items = [];
+    this.itemsNeeded = [];
+    this.name = '';
+    this.phone = '';
+    this.newName = '';
+    this.newBrand = '';
+    this.getRequestedItems();
+  }
+
+  getRequestedItems(): void {
+    this.searchService.getRequestedItems().subscribe(
+      data => { this.items = data; }
+      // err => console.error(err),
+      // () => console.log('done loading requested items')
+    );
+  }
+
+  onChange(item: Item, event) {
+    if (event.target.checked){
+      this.itemsNeeded.push(item);
+    } else {
+      for (let i = 0; i < this.itemsNeeded.length; i++) {
+        if (this.itemsNeeded[i].name === item.name) {
+          this.itemsNeeded.splice(i, 1);
+        }
+      }
+    }
+  }
+
+  addNewItem() {
+    // tslint:disable-next-line:triple-equals
+    if (this.newName != '' && this.newBrand != '') {
+      this.items.push({Name: this.newName, Brand: this.newBrand});
+    }
+  }
+}
