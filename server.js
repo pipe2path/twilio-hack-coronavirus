b5362998e176580b635e47969224e7ad3c4988a7
kevinmenezes <kevinmenezes@yahoo.com>
2020-04-20 11:32:01 -0700

    deploy configs-2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/twilio-hack-coronavirus'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/twilio-hack-coronavirus/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 const port = process.env.PORT || 8080;
