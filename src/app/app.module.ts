c5af458ab9fd1a8a478672eb591fb38508d6ce00
kevinmenezes <kevinmenezes@yahoo.com>
2020-04-24 00:41:17 -0700

    added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FoundComponent } from './found/found.component';
 import { SearchComponent } from './search/search.component';
+import { ImageComponent } from './image/image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    FoundComponent
+    FoundComponent,
+    ImageComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +26,7 @@ import { SearchComponent } from './search/search.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'found', component: FoundComponent },
       { path: 'search', component: SearchComponent },
+      { path: 'image', component: ImageComponent }
     ])
   ],
   providers: [],
